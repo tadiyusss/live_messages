265c23d1df45fa30b0dd60a3f682ed708397a5d8
Thaddeus Toledo <toledothaddeusf@gmail.com>
2026-09-08 13:59:06 +0800

    added a download leads button for administrators

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,7 +1,7 @@
 from flask_login import login_required
 from core.utils.decorators import roles_required
 from .. import bp
-from flask import render_template
+from flask import render_template, Response, url_for, redirect
 from ..models import LiveChatClient
 from core.extensions import db
     
@@ -11,3 +11,16 @@ from core.extensions import db
 def messages():
     clients = LiveChatClient.query.all()
     return render_template('dashboard/messages.html', clients=clients)
+
+@bp.route('/dashboard/live-messages/leads/download')
+@login_required
+@roles_required(['Administrator'])
+def download_leads():
+    leads = LiveChatClient.query.all()
+    headers = ['Full Name', 'Email', 'Phone Number']
+    data = [[lead.fullname, lead.email, lead.phone_number] for lead in leads]
+    return Response(
+        '\n'.join([','.join(headers)] + [','.join(map(str, row)) for row in data]),
+        mimetype='text/csv',
+        headers={'Content-Disposition': 'attachment; filename=leads.csv'}
+    )
